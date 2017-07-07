89ca3d0894db51a890099d8b3ecdc20ef4841f0c
Hubert Damien <damien@bruthe.com>
2017-07-07 15:23:25 +0200

    manage endpoints

--- index.js
+++ index.js
@@ -2,26 +2,39 @@ var http = require('http');
 var async = require('async');
 fs = require('fs')
 
-var config = {'myPort': 8080};
+var config = {
+	'myPort': 8080,
+	endPoints : []
+};
 
-if(!process.env.PING_PORTS){
+function getEndPointsFromArgs(args) {
+	return args.trim().split(';').map(function(url) { 
+		var splittedUrl = url.split(':');
+		return {
+			host : splittedUrl.length > 1 ? splittedUrl[0] : 'localhost',
+			port : splittedUrl.length === 0 ? splittedUrl[0] : splittedUrl[1],
+		}  
+	});
+}
+
+if(!process.env.PING_ENDPOINTS){
 	// read config file (please setup the right path)
 	fs.readFile('./config', 'utf8', function (err,data) {
   		if (err) {
     			console.log('error loading config file: '+err);
     			process.exit(-1);
   		}
-  		config.ports = data.trim().split(';');
-  		console.log('Will ping those ports: '+config.ports);
+  		config.endPoints = getEndPointsFromArgs(data);
+  		console.log('Will ping those endPoints: ' + config.endPoints);
 	});
 }else{
-	config.ports = process.env.PING_PORTS.trim().split(';');
-	console.log('Will ping those ports: '+config.ports);
+	config.endPoints = getEndPointsFromArgs(process.env.PING_ENDPOINTS);
+	console.log('Will ping those endPointss: ' + config.endPoints);
 }
 
-function getLocalPing(port) {
+function getLocalPing(endPoint) {
 	return function(callback) {
-		var request = http.request({host: 'localhost', port: port, path: '/ping', method: 'GET' } , function(res) {
+		var request = http.request({host: endPoint.host, port: endPoint.port, path: '/ping', method: 'GET' } , function(res) {
 			// error if statusCode != 200
 		    callback((res.statusCode != 200)?true:null, res.statusCode);
 		});
@@ -44,8 +57,8 @@ var server = http.createServer(function(request, response) {
 
 	// build list of functions to be executed in parallel
 	var localCalls = {};
-	for (var i in config.ports) {
-		localCalls[config.ports[i]] = getLocalPing(config.ports[i]);
+	for (var i in config.endPoints) {
+		localCalls[config.endPoints[i]] = getLocalPing(config.endPoints[i]);
 	}
 
 	// parallel execution of all the calls and send response when all terminated
